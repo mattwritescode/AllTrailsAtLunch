192695a861f3db335c9e16af523e39bbf2bcbc23
Matt Tyler <mtyler@eventbrite.com>
2021-12-03 11:18:14 -0600

    Initial Commit

---
+++ AllTrailsAtLunch/AllTrailsAtLunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllTrailsAtLunchApp.swift
+//  AllTrailsAtLunch
+//
+//  Created by Matthew Tyler on 12/3/21.
+//
+
+import SwiftUI
+
+@main
+struct AllTrailsAtLunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
